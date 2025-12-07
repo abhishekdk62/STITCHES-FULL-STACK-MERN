1fd8807c79613745bd4a39ddb2dd2d71aab32afe
ABHISHEK <abhidk62@gmail.com>
2025-12-07 22:40:08 +0530

    aded health

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,34 @@
-const { config } = require('dotenv');
+const { config } = require("dotenv");
 config();
-const morgan = require('morgan');
-const express = require('express');
-const connectDB = require('./config/db');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const session = require('express-session');
-const commonRouter = require('./routes/common/commonRoutes');
-const userRouter = require('./routes/user/userRoutes');
-const adminRouter = require('./routes/admin/adminRoutes');
-const paymentRouter = require('./routes/payment/paymentRoutes');
-const authRoutes = require('./routes/googleAuth/authRoutes');
+const morgan = require("morgan");
+const express = require("express");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const session = require("express-session");
+const commonRouter = require("./routes/common/commonRoutes");
+const userRouter = require("./routes/user/userRoutes");
+const adminRouter = require("./routes/admin/adminRoutes");
+const paymentRouter = require("./routes/payment/paymentRoutes");
+const authRoutes = require("./routes/googleAuth/authRoutes");
 
-const passport = require('./config/passport');
+const passport = require("./config/passport");
 
 const app = express();
 
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, 
+    origin: process.env.FRONTEND_URL,
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
-
 //!morgan code to see what all api calls coming from front end
 
-
 // app.use(morgan("dev"))
-
 
 app.use((req, res, next) => {
   res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
@@ -49,14 +46,14 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.use('/api/auth', authRoutes);
-app.use('/api/user', commonRouter);
-app.use('/api/user', userRouter);
-app.use('/api/admin', adminRouter);
+app.use("/api/health", (req, res) => {
+  res.status(200).json({ message: "ok", time: new Date() });
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/user", commonRouter);
+app.use("/api/user", userRouter);
+app.use("/api/admin", adminRouter);
 app.use("/api/api/paypal", paymentRouter);
-
-
 
 const PORT = process.env.PORT || 5000;
 connectDB();
