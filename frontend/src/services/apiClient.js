ff80c22510692665af7f5195bd5a570ba938a5b0
ABHISHEK <abhishek.autobse@gmail.com>
2026-06-29 13:26:36 +0530

    frontend storeref import fix

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getStore } from './storeRef';
+import { getStore } from '../storeRef';
 import { logout } from '../../slices/authSlice';
 
 const apiClient = axios.create({
